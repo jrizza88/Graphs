1ddc380055081512fb07e93bc7967cd3a3a3155f
Jamar Torres <Jetorres88@gmail.com>
2020-02-12 22:46:52 -0500

    solved BFS

--- graph2.py
+++ graph2.py
@@ -14,19 +14,13 @@ class Graph:
         Add a vertex to the graph.
         """
         self.vertices[vertex_id] = set()
-        #pass  # TODO
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        #self.vertices[v1].add(v2)
-        # this helps to check
         if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add[v2]
-        else:
-            raise IndexError("That vertex does not exists")
-        #pass  # TODO
+            self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
         """
@@ -34,67 +28,65 @@ class Graph:
         """
         return self.vertices[vertex_id]
 
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-    ## This whole thing takes the FIFO idea! First in First Out
-        #pass  # TODO
-        # algorithm 
-        # they will not hold true? necessarily
-        # algorithm will hold true!
-
-        ## Create an empty queue and enqueue the starting vertex ID
-        q = Queue()
-
-        q.enqueue(starting_vertex)
-        ## Create an empty Set to store visited nodes/verticies
+        # make a queue
+        queue = Queue()
+        # make a set for the visited nodes
         visited = set()
-        # visited = {1}
-        # visited = {1, 2}
-        ## While the queue is not empty...
-        while q.size() > 0: 
-            # Dequeue the first vertex
-            current_node = q.dequeue()
-            # If that vertex has not ben visited...
-            if current_node not in visited:
-                # Mark it as visited.
-                print(current_node)
-                visited.add(current_node)
-                # Then add all of its neighbors to the back of the queue
-                for neighbor in self.vertices[current_node]:
-                    q.enqueue(neighbor)
 
-                # ALT way to get neighbors and then put them in lines:
-                ## and get its neighbors
-                #edges = self.get_neighbors(current_node)
-                ## put them in line to be visited
-                #for edge in edges:
-                 #   queue.enqueue(edge)
+        # put our starting node in line
+        queue.enqueue(starting_vertex)
+
+        # if our queue's not empty, we have more people to visit!
+        while queue.size() > 0:
+            # get the next node out of line
+            current_node = queue.dequeue()
+
+            # check if it has been visited
+            if current_node not in visited:
+            ## if not, mark as visited
+                visited.add(current_node)
+                print(current_node)
+            ## and get its neighbors
+                edges = self.get_neighbors(current_node)
+            ## put them in line to be visited
+                for edge in edges:
+                    queue.enqueue(edge)
+
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        ## Create an empty stack and push the starting vertex ID
-        s = Stack()
-        s.push(starting_vertex)
-        ## Create an empty Set to store visited vertices
+        # make a stack
+        stack = Stack()
+        # make a set for the visited nodes
         visited = set()
-        ## While the stack is not empty...
-        while s.size() > 0: 
-            # Pop the first vertex
-            v = s.pop()
-            # If that vertex has not bexsen visited...
-            if v not in visited:
-                # Mark it as visited.
-                print(v)
-                visited.add(v)
-                # Then add all of its neighbors (push) to the top of the stack
-                for neighbor in self.vertices[v]:
-                    s.push(neighbor)
-        #pass  # TODO
+
+        # put our starting node on top of the stack
+        stack.push(starting_vertex)
+
+        # if our stack's not empty, we have more people to visit!
+        while stack.size() > 0:
+            # get the next node off the top of our stack
+            current_node = stack.pop()
+
+            # check if it has been visited
+            if current_node not in visited:
+            ## if not, mark as visited
+                visited.add(current_node)
+                print(current_node)
+            ## and get its neighbors
+                edges = self.get_neighbors(current_node)
+            ## stack them on the stack to be visited
+                for edge in edges:
+                    stack.push(edge)
+
 
     def dft_recursive(self, starting_vertex):
         """
@@ -111,43 +103,22 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
         # make a queue
-        queue = Queue()
         # make a set for visited
-        visited = set()
-        # enqueue A PATH TO the starting_vertex
-        queue.enqueue([starting_vertex])
-        # while the queue isn't empty:
-        while queue.size() > 0:
-        ## dequeue the next path
 
-            path = queue.dequeue()
-            ## current_node is the last thing in the path
-            ## -1 helps to get the last instance
-            current_node = path[-1]
-            ## check if it's the target, aka the destination_vertex
-            ## if so, return the path!!
-            ## -1 helps to get the last instance
-            if current_node == destination_vertex:
-                print('when current node = destination', path)
-                return path
+        # enqueue A PATH TO the starting_vertex
+
+        # while the queue isn't empty:
+        ## dequeue the next path
+        ## current_node is the last thing in the path
+        ## check if it's the target, aka the destination_vertex
+        ## if so, return the path!!
 
         ## if not, mark this as visited
-            if current_node not in visited:
-                visited.add(current_node)
         ## get the neighbors
-                edges = self.get_neighbors(current_node)
         ## copy the path, add the neighbor to the copy
-               # copy_path = self.get_neighbors(current_node)
-               # copy_path.add(path)
         ## for each one, add a PATH TO IT to our queue
-                for edge in edges:
-                    #copy_path = path[:]
-                    copy_path = path.copy()
-                    copy_path.append(edge)
-                    queue.enqueue(copy_path)
-        return[]
+
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -177,16 +148,16 @@ if __name__ == '__main__':
     graph.add_vertex(5)
     graph.add_vertex(6)
     graph.add_vertex(7)
-    #graph.add_edge(5, 3)
-   # graph.add_edge(6, 3)
-    #graph.add_edge(7, 1)
-    #graph.add_edge(4, 7)
-    #graph.add_edge(1, 2)
-    #graph.add_edge(7, 6)
-    #graph.add_edge(2, 4)
-    #graph.add_edge(3, 5)
-    #graph.add_edge(2, 3)
-    #graph.add_edge(4, 6)
+    graph.add_edge(5, 3)
+    graph.add_edge(6, 3)
+    graph.add_edge(7, 1)
+    graph.add_edge(4, 7)
+    graph.add_edge(1, 2)
+    graph.add_edge(7, 6)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 5)
+    graph.add_edge(2, 3)
+    graph.add_edge(4, 6)
 
     '''
     Should print:
@@ -209,7 +180,6 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    print(graph.bft(1))
     graph.bft(1)
 
     '''
